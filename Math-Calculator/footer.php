ed6dc57a57231eee498518c8ed0e4c1240d7cfe4
sweetchuku <86880763+sweetchuku@users.noreply.github.com>
2022-04-26 12:44:08 +0530

    Add files via upload

---
+++ Math-Calculator/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Math_Calculator
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			
+			<span class="sep"> </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
